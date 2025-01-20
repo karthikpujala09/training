237bf1711dc93f395cbeeb3d26d0854d89d69b33
lordvoldemort6969 <158834908+lordvoldemort6969@users.noreply.github.com>
2025-01-20 11:41:57 +0530

    pushing the code to repo

---
+++ Batch2/arrays.js
@@ -0,0 +1,122 @@
+// let user1 = "x";
+// let user2 = "y";
+// let user3 = "z";
+// let user4 = "a";
+// let user5 = "b";
+
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+// console.log(user5);
+
+
+
+
+// let users = ["x" , "y" , "z" , "p"];
+//  console.log(users[0]);
+//  console.log(users[1]);
+//  console.log(users[2]);
+//  console.log(users[3]);
+
+
+// for (let i=0;i<users.length;i++){
+//     console.log(users[i]);
+// }
+
+
+
+// let users = ["x", "y", "z", "p", "a"];
+
+// users.map((user ,i)=>{
+//     console.log(user, i)
+// })
+
+// let x =users.map((user ,i)=>{
+//     return user;
+
+// })
+// console.log(x);
+
+// let x = users.forEach((user,i)=>{
+//     return user;
+// })
+// console.log(x);
+
+
+// let userDetails = {
+//     name : "Akshay",
+//     age : "22",
+//     address:{
+//         city : "Hyderabad",
+//         area : "siddipet",
+//         streetNo : 123;
+//         pincode : {
+//             pin1:123456,
+//             pin2:654321,
+//         }
+//     }
+    
+// };
+
+// console.log(userDetails.name);
+// console.log(userDetails,address,city);
+// console.log(userDetails.address.pincode.pin1);
+
+//  create array of objects
+// let user1 ={
+//     name:"X",
+//     city:"abc"
+// }
+
+// let user2 ={
+//     name:"y",
+//     city:"qwe"
+// }
+
+// let user3 ={
+//     name:"Z",
+//     city:"rty"
+// }
+
+
+// let user4 ={
+//     name:"A",
+//     city:"uio"
+// }
+
+// console.log(user1.name);
+
+let userDetails =[
+    {
+      
+            name:"X",
+            city:"abc"
+        
+    }
+
+    {
+        
+            name:"y",
+            city:"qwe"
+        
+    }
+
+    {
+        
+            name:"Z",
+            city:"rty"
+        
+    }
+
+
+    {
+        
+            name:"A",
+            city:"uio"
+        
+    }
+
+];
+
