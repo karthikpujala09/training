c50cc17e65b8ea2bf0d62022875b9b21bb034a63
lordvoldemort6969 <158834908+lordvoldemort6969@users.noreply.github.com>
2025-01-24 11:42:51 +0530

    pushing the code to repo

---
+++ Batch2/dom.js
@@ -0,0 +1,37 @@
+// let element = document.getElementById("demo");
+
+// element.innerHTML = "DOM"
+// console.log(element);
+
+// let test = document.getElementById("test");
+// test.innerHTML = "<h1>Header</h1>"
+// console.log(test);
+
+// // !
+// let ele = document.getElementsByClassName("test");
+// ele[0].style.backgroundColor="dodgerblue"
+// console.log(ele);
+// console.log(Array.isArray(ele));
+
+
+// let x = [...ele];
+// console.log(x , Array.isArray(x))
+// x.map(element=>{
+//     element.style.backgroundColor="tomato";
+//     element.style.color="white";
+//     element.style.testTransform = "uppercase";
+
+// })
+
+// let ele = document.getElementsByTagName("div");
+// console.log(ele);
+
+
+// let ele = document.querySelector("#demo");
+// console.log(ele);
+// let ele = document.querySelector(".test");
+// console.log(ele);
+
+
+
+
