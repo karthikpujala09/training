44d6cb8c7570bb5226100f443afcbd2620bd203e
lordvoldemort6969 <158834908+lordvoldemort6969@users.noreply.github.com>
2025-01-31 11:29:09 +0530

    pushing the code to repo

---
+++ React/child1.js
@@ -0,0 +1,21 @@
+// let x = 10;
+// export default x;
+
+// let userDetail={
+//     name:"Sathvik",
+//     Profession:"CR"
+// }
+// export default userDetail;
+
+
+let x = 10;
+const user = "p";
+const users = ["a", "b", "c", "d", "e"];
+const obj = {
+    name: "sam",
+    company: "ExcelR"
+};
+export { x, user, users, obj };
+
+
+export default "This is a default export";
